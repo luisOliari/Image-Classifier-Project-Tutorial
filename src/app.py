cc2651a396d31b8b3eb80d1ca88bc76aa8501ac3
luisOliari <luisoliari@yahoo.com>
2022-08-07 18:59:20 +0000

    My first commit

--- src/app.py
+++ src/app.py
@@ -2,3 +2,78 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+
+# importar librerias:
+from tensorflow import keras
+import keras,os
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from matplotlib import pyplot
+from matplotlib.image import imread
+
+# plot dog photos from the dogs vs cats dataset
+# define location of dataset
+folder = "../data/raw/train/sub_train/dog/"
+#extract_dir1 = "../data/raw/test"
+#folder = 'train/'
+# plot first few images
+for i in range(0,9):
+# define subplot
+    pyplot.subplot(330 + 1 + i)
+    # define filename
+    filename = folder + 'dog.' + str(i) + '.jpg'
+    # load image pixels
+    image = imread(filename)
+    # plot raw pixel data
+    pyplot.imshow(image)
+    # show the figure
+    pyplot.show()
+
+# Use the ImageDataGenerator to load the data
+trdata = ImageDataGenerator()
+traindata = trdata.flow_from_directory(directory="../data/raw/train/sub_train/",target_size=(224,224))
+tsdata = ImageDataGenerator()
+testdata = tsdata.flow_from_directory(directory="../data/raw/train/sub_test/", target_size=(224,224))
+
+# pasar la data por la red neuronal
+model = Sequential()
+model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+# Pasar la data por la capa densa 
+model.add(Flatten())
+model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=2, activation="softmax"))
+
+# compilar el modelo:
+from keras.optimizers import Adam
+opt = Adam(lr=0.001)
+model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
+
+# obtener el resumen del modelo:
+model.summary()
+
+# Create objects for ModelCheckpoint and EarlyStopping, and pass them as callback functions to fit_generator
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+early = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=1, mode='auto')
+hist = model.fit_generator(steps_per_epoch=100,generator=traindata, validation_data= testdata, validation_steps=10,epochs=100,callbacks=[checkpoint,early])
